500e6d742d55c00e8f60260d40b69959a81489b3
jeongsikwoo <mcknight1103@gmail.com>
2017-11-20 10:32:37 +0900

    Chatting program using socket networking

---
+++ Assets/Scripts/Chat.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Chat : MonoBehaviour {
+
+	// 보낸 채팅 결과
+	public List<string> chatHistory = new List<string>();
+	// 현재 메시지
+	private string currentMessage = string.Empty;
+
+	private void SendMassage()
+	{
+		if(!string.IsNullOrEmpty(currentMessage.Trim ()))
+		{
+			networkView.RPC ("ChatMessage", RPCMode.AllBuffered, new object[]{currentMessage});
+			currentMessage = string.Empty;
+		}
+	}
+
+	private void Bottom()
+	{
+		currentMessage = GUI.TextField (new Rect (0, Screen.height - 20, 500, 20), currentMessage);
+		if (GUI.Button (new Rect (525, Screen.height - 20, 75, 20), "Send"))
+			SendMassage();
+
+		GUILayout.Space (15);
+		for (int i=chatHistory.Count - 1; i>=0; i--)
+			GUILayout.Label (chatHistory [i]);
+	}
+
+	private void Top()
+	{
+		GUILayout.Space (15);
+		GUILayout.BeginHorizontal (GUILayout.Width (250));
+		currentMessage = GUILayout.TextField (currentMessage);
+		if (GUILayout.Button ("Send"))
+			SendMassage();
+		GUILayout.EndHorizontal ();
+		
+		foreach (string c in chatHistory)
+			GUILayout.Label (c);
+	}
+
+	private void OnGUI()
+	{
+		if (!NetworkMenu.Connected)
+						return;
+
+		Bottom ();
+		}
+
+	[RPC]
+	public void ChatMessage(string message)
+	{
+		chatHistory.Add (message);
+	}
+}
---
+++ Assets/Scripts/NetworkMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkMenu : MonoBehaviour {
+
+	// 접속 IP
+	public string connectionIP = "127.0.0.1";
+	// 접속 포트번호
+	public int portNumber = 1000;
+	// 접속 여부
+	public static bool Connected { get; private set;}
+
+	private void OnConnectedToServer()
+	{
+		// Client가 연결됨
+		Connected = true;
+	}
+
+	private void OnServerInitialized()
+	{
+		// Server가 생성됨
+		Connected = true;
+	}
+
+	private void OnDisconnectedFromServer()
+	{
+		// 연결이 끈어지거나, 누락
+		Connected = false;
+	}
+
+	private void OnGUI()
+	{ // 현재 사용자의 네트워크에 접속 여부 판단
+		if (!Connected) {
+			connectionIP = GUILayout.TextField (connectionIP);
+			int.TryParse (GUILayout.TextField (portNumber.ToString ()), out portNumber);
+
+			// 채팅에 접속하는 버튼
+			if (GUILayout.Button ("Connect")){
+				// 채팅 서버 접속 : Connect(접속IP, 접속포트번호)
+				Network.Connect (connectionIP, portNumber);
+
+			}
+			// 채팅을 생성하는 버튼
+			if (GUILayout.Button ("Server"))
+				// 채팅 서버 생성 : InitializeServer(접속자수, 포트번호, NAT사용여부)
+				Network.InitializeServer (10, portNumber, true);
+		}
+		else
+			// 접속 상태일때 메시지 출력
+			GUILayout.Label ("Client Count : " + Network.connections.Length.ToString ());
+
+	}
+}
